7382b30d162cb55740b9be6f60030d8cc8d15631
Josiah P <vasti009@gmail.com>
2016-01-23 19:22:48 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 name="eventity",
-version='0.1.3',
+version='0.2.0',
 description="Easy event based ECS system",
 keywords="event entity component system ecs",
 url="https://github.com/JonSnowbd/Eventity",
